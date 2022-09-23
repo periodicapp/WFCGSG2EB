686a108f6751c1ecba0dbfaa669a982ba2bec835
Austin Parks <austinparks207@gmail.com>
2022-09-23 09:26:07 -0400

    Week 2 Solution + Updated Readme

--- ALP/README.md
+++ ALP/README.md
@@ -5,12 +5,16 @@ These are the solutions for WFCGSG2EB problems by Austin Parks
 ## How to Run
 
 All solutions in Python.
-As of 09/2022 solutions were created in Python 3.10.1
+As of 09/2022 solutions were created in [Python 3.10.1](https://www.python.org/downloads/release/python-3101/)
 
 ## Manifest
 
-1. *Week 1 - 2022-09-16 - 4sum*:
-   1. Contains solutions to 2sum, 3sum and 4sum/ksum.
-      1. 2sum is implemented utilizing a dictionary, and a simple for-loop returning indexes of combinations.
-      2. 3sum is implemented utilizing a 2 pointer system on a sorted list with varying conditions for incrementing and decrementing based on the sum of the current pointers
-      3. 4sum utilizes a combination of 3sum and 2sum to make a solution that can be utilized against any length of requested combinations. Here it is used to make a 4sum combo, but could also be utilized to make a 5sum, 6sum, 7sum, etc... It is implemented with a naturally recursive function ksum that passes to the 2sum helper.
+1. *[Week1.py](Week1.py) - 2022-09-16 - [4sum](https://leetcode.com/problems/4sum/)*
+   1. Contains solutions to [2sum](https://leetcode.com/problems/two-sum/), [3sum](https://leetcode.com/problems/3sum/) and [4sum/ksum](https://leetcode.com/problems/4sum/)
+      1. [2sum](https://leetcode.com/problems/two-sum/) is implemented utilizing a dictionary, and a simple for-loop returning indexes of combinations.
+      2. [3sum](https://leetcode.com/problems/3sum/) is implemented utilizing a 2 pointer system on a sorted list with varying conditions for incrementing and decrementing based on the sum of the current pointers
+      3. [4sum](https://leetcode.com/problems/4sum/) utilizes a combination of 3sum and 2sum to make a solution that can be utilized against any length of requested combinations. Here it is used to make a 4sum combo, but could also be utilized to make a 5sum, 6sum, 7sum, etc... It is implemented with a naturally recursive function ksum that passes to the 2sum helper.
+2. *[Week2.py](Week2.py) - 2022-09-23 - [Longest Palindrome](https://leetcode.com/problems/longest-palindromic-substring/solution/)*
+   1. Contains 2 solutions to [Longest Palindrome](https://leetcode.com/problems/longest-palindromic-substring/solution/)
+      1. Brute Force Algorithm
+      2. LeetCode's "Dynamic Programming" Algorithm
---
+++ ALP/Week2.py
@@ -0,0 +1,70 @@
+class Solution:
+    # Brute Force
+    def longestPalindromeBrute(self, s: str) -> str:
+        strList = list(s)
+        longestPalindrome = []
+        # Finds every possible Palindrome and compares to the current longest palindrome
+        for x in range(len(strList)):
+            currPal = []
+            revPal = []
+            next = x
+            # Here is the downside of the method: It checks the rest of the string starting at every index, and everytime, 
+            # it checks if the current possible palindrome is indeed a palindrome and if it is longer than the current longest
+            # aba: [a], [ab], [aba];  [b], [ba];  [a]; [aba] > [ba] > [a]
+            while next < len(strList):
+                currPal.append(strList[next])
+                revPal.insert(0, strList[next])
+                if revPal == currPal:
+                    if len(longestPalindrome) < len(currPal):
+                        longestPalindrome = currPal.copy()
+                next += 1
+        return ''.join(longestPalindrome)
+    
+    # Modifed Solution from LeetCode's Solution #4: Java -> Python
+    def longestPalindrome(self, s: str) -> str:
+        # Finds the center of the first palindrome
+        def expandCenter(s: str, left: int, right: int) -> int:
+            while(left >= 0 and right < len(s) and s[left] == s[right]):
+                left -= 1
+                right += 1
+            # Length of palindrome
+            return right - left - 1
+        
+        # Indexes of longest Palindrome
+        start, end = 0,0
+        for i in range(len(s)):
+            # For every index, look for a palindrome of odd length: Base case is 1
+            len1 = expandCenter(s, i, i)
+            # Palindrome of even length: Base Case is 2
+            len2 = expandCenter(s, i, i+1)
+            # Is the odd length palindrome longer than the even length palindrome 
+            if len1 > len2:
+                leng = len1
+            else:
+                leng = len2
+            # Is the current palindrome longer than the previous longest
+            if leng > end - start:
+                # aba at b: 1 - (1 - 1) // 2 = 0 
+                start = i - (leng - 1) // 2
+                # aba at b: 1 + 1 // 2 = 1
+                end = i + leng // 2
+        # aba: start = 0, end = 1, s[0, 1 + 1 (2)] = 2 = "aba"
+        return s[start:end+1]
+    
+    
+        
+    
+a = Solution()
+print(a.longestPalindromeBrute("a"))
+print(a.longestPalindromeBrute("ab"))
+print(a.longestPalindromeBrute("aa"))
+print(a.longestPalindromeBrute("aab"))
+print(a.longestPalindromeBrute("aba"))
+print(a.longestPalindromeBrute("baa"))
+print(a.longestPalindromeBrute("abc"))
+print(a.longestPalindromeBrute("aacadkbacaa"))
+print(a.longestPalindromeBrute("aabdhudehewopdk"))
+print(a.longestPalindromeBrute("dhwpkwqjaahuwehiewklwq"))
+test = ''.join(['o' for x in range(1700)])
+print(a.longestPalindromeBrute(test))
+print(a.longestPalindrome(test))
