e49a7136731477f3b59f697350ff11bba7a26c7d
Joshua Herring <jwherring@gmail.com>
2022-09-27 06:03:30 -0400

    WIP for binary search trees ii

---
+++ JWH/Week-3/bintree.hs
@@ -0,0 +1,42 @@
+
+data BinTree a = Empty | Branch a (BinTree a) (BinTree a)
+  deriving (Show)
+
+construct' :: Int -> [Int] -> [Int] -> BinTree Int
+construct' x l r
+  | l == [] && r == [] = Branch x Empty Empty 
+  | l == [] = Branch x Empty (construct r)
+  | r == [] = Branch x (construct l) Empty
+  | otherwise = Branch x (construct l) (construct r)
+
+construct :: [Int] -> BinTree Int
+construct (x:xs) = 
+  let 
+    left = filter (<x) xs
+    right = filter (>x) xs
+  in
+    construct' x left right 
+
+treeNode :: BinTree Int -> Int
+treeNode t =
+  case t of
+    (Branch x _ _) -> x
+    Empty -> 0
+
+children :: BinTree Int -> [Int]
+children t =
+  case t of
+    (Branch x Empty Empty) -> [] 
+    (Branch x y Empty) -> toArray y
+    (Branch x Empty y) -> [0] ++ toArray y
+    (Branch x y z) -> [treeNode y, treeNode z] ++ children y ++ children z
+
+toArray :: BinTree Int -> [Int]
+toArray t = 
+  case t of
+    (Branch x Empty y) -> [x,0] ++ toArray y
+    (Branch x y Empty) -> [x] ++ [treeNode y] ++ children y ++ [0]
+    (Branch x y z) -> [x] ++ [treeNode y, treeNode z] ++ children y ++ children z
+    Empty -> [0]
+
+main = print $ toArray $ construct [3,4,1,5,2]
