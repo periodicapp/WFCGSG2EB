ebd28b790b0202ae3aa4d044857e3765c5a69300
Joshua Herring <jwherring@gmail.com>
2022-10-13 07:31:44 -0400

    track index on maxProfitRight

--- JWH/Week-5/stonks-ii.hs
+++ JWH/Week-5/stonks-ii.hs
@@ -3,26 +3,31 @@ import System.Random
 getList :: Int -> Int -> StdGen -> [Int]
 getList n l g = take n (randomRs (0::Int, l) g)
 
-maxProfitsRight :: Int -> Int -> [Int] -> [Int]
-maxProfitsRight _ mpf [] = [] 
-maxProfitsRight lw mpf (l:ls) = 
+maxProfitsRight :: Int -> Int -> Int -> Int -> [(Int,Int)] -> [(Int,Int,Int)]
+maxProfitsRight _ _ _ _ [] = [] 
+maxProfitsRight lw mpf mn h ((j,l):ls) = 
   let
     low = min l lw
+    minindex = if l < lw then j else mn
     mp = max (l - lw) mpf
+    maxindex = if l - lw > mpf then j else h
   in
-    mp : (maxProfitsRight low mp ls)
+    (mn,maxindex,mp) : (maxProfitsRight low mp minindex maxindex ls)
 
-mp :: Int -> Int -> Int -> [(Int,Int)] -> [Int] -> Int
+thrd :: (Int,Int,Int) -> Int
+thrd (_,_,e) = e
+
+mp :: Int -> Int -> Int -> [(Int,Int)] -> [(Int,Int,Int)] -> Int
 mp  _ _ fp [] _ = fp
 mp h mpp fp ((i,l):ll) rr = 
   let
     hg = max h l
     maxprf = max (h - l) mpp
-    finalprofit = max fp (maxprf + rr !! i)
+    finalprofit = max fp (maxprf + (thrd $ rr !! i))
   in
     mp hg maxprf finalprofit ll rr
 
-maxProfit :: [Int] -> [Int] -> Int
+maxProfit :: [Int] -> [(Int,Int,Int)] -> Int
 maxProfit ll rr = mp 0 0 0 (reverse $ zip [0..] ll) rr
 
 --main = print $ maxProfitsRight 5 0 [5,9,1,2,7,3,8,3,6,5]
@@ -30,6 +35,7 @@ main = do
   g <- getStdGen
   let stocks = getList 10 10 g
   print $ stocks
-  print $ maxProfit stocks $ maxProfitsRight (head stocks) 0 stocks
+  print $ maxProfitsRight (head stocks) 0 0 0 (zip [0..] stocks)
+  print $ maxProfit stocks $ maxProfitsRight (head stocks) 0 0 0 (zip [0..] stocks)
 
 
