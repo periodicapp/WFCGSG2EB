7183ab0dbff2f3a581b7dd0ab4378a9cce398181
Joshua Herring <josh@periodic.is>
2022-10-18 12:52:31 -0400

    remove superfluous comments

--- JWH/Week-6/pairs.hs
+++ JWH/Week-6/pairs.hs
@@ -58,9 +58,4 @@ x <||> y
 arrangeList :: [(Int,Int)] -> Maybe [(Int,Int)]
 arrangeList ls = foldl1 (<||>) $ map arrangePairs $ allHeads ls ls
 
---main = print $ validTails (11,9) [(4,5),(11,9),(9,4)]
---main = print $ Nothing <||> Just [(1,2)]
---main = displayPairs $ arrangePairs ((11,9),[(4,5),(9,4)])
---main = displayPairs $ arrangePairs ((11,9),[(5,1),(4,5),(9,4)])
---main = displayPairs $ foldl1 (<||>) $ map arrangePairs $ allHeads [(11,9),(5,1),(4,5),(9,4)] [(11,9),(5,1),(4,5),(9,4)]
 main = displayPairs $ arrangeList [(11,9),(5,1),(4,5),(9,4)]
