e3e93383f5fed17290099db2f31c0c88d5f165e9
Joshua Herring <josh@periodic.is>
2022-10-18 08:53:14 -0400

    implement arrangePairs - complete solution

--- JWH/Week-6/pairs.hs
+++ JWH/Week-6/pairs.hs
@@ -9,18 +9,25 @@ del x (l:ls)
 --be the tail) and, for each item in the first list, prepends it to the second
 --list, taking care that the item being prepended has been removed (at least,
 --that the first instance of it) from the second list before prepending.
-allHeads :: [(Int,Int)] -> [(Int,Int)] -> [[(Int,Int)]]
+allHeads :: [(Int,Int)] -> [(Int,Int)] -> [((Int,Int),[(Int,Int)])]
 allHeads [] _ = []
-allHeads (a:as) b = (a : (del a b)) : allHeads as b
+allHeads (a:as) b = (a , del a b) : allHeads as b
 
---arrangePairs - the main function.  Flesh this out.
-arrangePairs :: [(Int,Int)] -> [[(Int,Int)]]
-arrangePairs [] = []
-arrangePairs ((s,e):ps) = 
+mapp :: (Int,Int) -> Maybe [(Int,Int)] -> Maybe [(Int,Int)]
+mapp (x,y) (Just zs) = Just ((x,y) : zs)
+mapp (x,y) Nothing = Nothing
+
+arrangePairs :: ((Int,Int),[(Int,Int)]) -> Maybe [(Int,Int)]
+arrangePairs ((x,y),[]) = Just [(x,y)]
+arrangePairs ((x,y),ls) =
   let
-    tails = allHeads (filter ((==e) . fst) ps) ps
+    conts = filter ((==y) . fst) ls
   in
-    map ((s,e):) tails
+    if (length conts) == 0 then Nothing else (x,y) `mapp` (foldl1 (<||>) $ map arrangePairs $ (flip allHeads $ ls) conts)
+    
+conts :: ((Int,Int),[(Int,Int)]) -> [(Int,Int)]
+conts (_,[]) = []
+conts ((x,y),ls) = filter ((==y) . fst) ls
 
 displayPairs :: Maybe [(Int,Int)] -> IO ()
 displayPairs (Just ps) = print ps
@@ -34,5 +41,5 @@ x <||> y
 --main = print $ validTails (11,9) [(4,5),(11,9),(9,4)]
 --main = print $ Nothing <||> Just [(1,2)]
 --main = displayPairs $ arrangePairs [(11,9),(4,5),(9,4)]
-main = print $ arrangePairs [(11,9),(9,1),(4,5),(9,4)]
+main = print $ arrangePairs ((11,9),[(5,1),(4,5),(9,4)])
 
