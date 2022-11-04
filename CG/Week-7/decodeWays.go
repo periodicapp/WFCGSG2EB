36542b0a7ce971582b1a2ca04176447e877d5c1e
claygrumeo <48027365+claygrumeo@users.noreply.github.com>
2022-11-04 10:05:11 -0400

    Update decodeWays.go

--- CG/Week-7/decodeWays.go
+++ CG/Week-7/decodeWays.go
@@ -7,6 +7,9 @@ import(
 
 // This does not work for larger strings of numbers.  It doens't pass the leetcode submission
 // I see there's super shorter ways to do this online but i wasn't understanding those.
+// The recusion part is wrong and it doesn't get all the possibilities when the string gets big.  
+// I can't really explain how it's meant to work as I was just trying a lot of things to 
+// try and get it to work.
 
 func main() {
 	fmt.Println("numDecodings('12')", numDecodings("12"))
